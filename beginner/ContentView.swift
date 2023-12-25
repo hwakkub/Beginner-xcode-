12136eb3c7ec231d7ebf8f0f638904d9aaa92b20
hwakkub <152359933+hwakkub@users.noreply.github.com>
2023-12-25 14:14:39 +0700

    Initial Commit

---
+++ beginner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  beginner
+//
+//  Created by jirakit on 25/12/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ beginner/beginnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  beginnerApp.swift
+//  beginner
+//
+//  Created by jirakit on 25/12/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct beginnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
