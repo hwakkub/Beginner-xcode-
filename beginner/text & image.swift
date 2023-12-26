2359af3a2ce29609a1a0d983c4f0c412d8a923cc
hwakkub <152359933+hwakkub@users.noreply.github.com>
2023-12-26 22:59:29 +0700

    Mapkit

---
+++ beginner/text & image.swift
@@ -0,0 +1,18 @@
+//
+//  text & image.swift
+//  beginner
+//
+//  Created by jirakit on 26/12/2566 BE.
+//
+
+import SwiftUI
+
+struct text___image: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    text___image()
+}
