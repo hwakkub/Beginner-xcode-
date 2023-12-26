cf89dd4261f95b8bd4aa641f644decaab5508620
hwakkub <152359933+hwakkub@users.noreply.github.com>
2023-12-26 23:03:25 +0700

    Mapkit (re)

--- beginner/ContentView.swift
+++ beginner/ContentView.swift
@@ -9,15 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack (alignment:.leading){
+        VStack {
+            MapView()
+                .frame(height: 300)
+            
+            
             Text("Hello, world!")
                 .font(.title)
                 .fontWeight(.heavy)
                 .foregroundColor(.green)
-            Text("My name is Jirakit")
-                .font(.subheadline)
-                .foregroundColor(.green)
+            Image("background")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 150, height: 150)
+                .clipShape(Circle())
+                .overlay {
+                    Circle().stroke(Color.gray, lineWidth: 4)
+                }
+                .shadow(radius: 7)
             
+            HStack {
+                Text("My name is")
+                    .font(.subheadline)
+                Spacer()
+                Text("Jirakit")
+                    .font(.subheadline)
+            }
+            .padding()
+
         }
         .padding()
     }
--- beginner/text & image.swift
+++ beginner/text & image.swift
@@ -8,9 +8,34 @@
 import SwiftUI
 
 struct text___image: View {
+//    this code
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Hello, world!")
+                .font(.title)
+                .fontWeight(.heavy)
+                .foregroundColor(.green)
+            Image("background")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 150, height: 150)
+                .clipShape(Circle())
+                .overlay {
+                    Circle().stroke(Color.gray, lineWidth: 4)
+                }
+                .shadow(radius: 7)
+            HStack {
+                Text("My name is")
+                    .font(.subheadline)
+                Spacer()
+                Text("Jirakit")
+                    .font(.subheadline)
+            }
+            .padding()
+        }
+        .padding()
     }
+    // end
 }
 
 #Preview {
