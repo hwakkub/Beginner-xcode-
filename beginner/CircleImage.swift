c1fc21bf5a2a562d65eb9b35c25c4e8091473371
hwakkub <152359933+hwakkub@users.noreply.github.com>
2023-12-26 23:20:03 +0700

    Creating and combining views (end)

---
+++ beginner/CircleImage.swift
@@ -0,0 +1,18 @@
+//
+//  CircleImage.swift
+//  beginner
+//
+//  Created by jirakit on 26/12/2566 BE.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
